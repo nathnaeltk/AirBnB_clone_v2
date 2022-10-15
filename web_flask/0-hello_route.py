0fa5ede61187b5d7d01d4124de8ed14b925c9cdf
nathnaeltk <hellonathnael@gmail.com>
2022-10-15 11:20:52 +0300

    submit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+    Starts a Flash web application
 """
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    """
+        Prints `Hello HBNB!` when accessing root of website
+    """
+    return 'Hello HBNB!'
 
-@app.route("/")
-def index():
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+app.run(host='0.0.0.0', port='5000')
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,1 +1,1 @@
-# Web flask
+# AirBnB clone - Web framework
