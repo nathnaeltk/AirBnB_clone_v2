bfffa94d85b901f5f7e2eb24f087f568f39107c6
nathnaeltk <hellonathnael@gmail.com>
2022-10-15 11:21:49 +0300

    submit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,14 +2,20 @@
 """
 starts a Flask web application
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
 if __name__ == "__main__":
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,1 +1,1 @@
-# AirBnB clone - Web framework
+# Web flask
